9b84b01838008a58cb4c6b74843e370cfec71136
Christian Toftgaard Pedersen <85504818+ChristianTP123@users.noreply.github.com>
2023-04-11 15:48:00 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
         rel="stylesheet"
       />
+      <meta name="google-site-verification" content="nIfz5eFECnlwo-uBe0hf7ia_zG8Bmgm-sDtDg8jWRek" />
     </Head>
     <script async src="https://cdn.splitbee.io/sb.js"/>
     <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" />
